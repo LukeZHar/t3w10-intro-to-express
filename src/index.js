205374adea7fb8de045d6288d89885e8cf4f8e03
LukeZHar <15649@coderacademy.edu.au>
2025-02-09 12:03:38 +1000

    Feat: Added a POST request

--- src/index.js
+++ src/index.js
@@ -8,19 +8,27 @@ const app = express();
 const PORT = 3300;
 
 // Our URL: localhost:PORTNUMBER/url_name
-app.get("/", (req, res)=> {
+app.get("/", (req, res) => {
     // Function logic executed
     // res.send("<h1>Hello from express backend</h1>");
     // res.sendStatus(201);
-    res.status(201).send("<h1>Hello from express backend</h1>");
+    res.status(500).send("<h1>Hello from express backend</h1>");
+    // res.download("./src/index.js");
 });
 
-app.get("/users", (req, res)=> {
+app.get("/users", (req, res) => {
     // Function logic executed
     res.json({
         message: "JSON response sent!"
     });
 });
+
+app.post("/", (req, res) => {
+    // Function logic executed
+    res.json({
+        message: "POST Request made!"
+    });
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
